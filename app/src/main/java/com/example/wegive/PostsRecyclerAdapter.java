7ae35bad849a36a11aac168a1bef80902b5fc5a1
AfikNof95 <afiknof9@gmail.com>
2023-03-10 19:13:15 +0200

    Attend to event

--- app/src/main/java/com/example/wegive/PostsRecyclerAdapter.java
+++ app/src/main/java/com/example/wegive/PostsRecyclerAdapter.java
@@ -3,6 +3,7 @@ package com.example.wegive;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,42 +11,96 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wegive.models.post.Post;
+import com.example.wegive.models.post.PostModel;
+import com.example.wegive.models.user.User;
+import com.google.android.material.chip.Chip;
 import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 class PostViewHolder extends RecyclerView.ViewHolder {
 
     List<Post> data;
     TextView postTitle;
     TextView postContent;
+    TextView postUserName;
     ImageView postImage;
+    ImageView userAvatar;
+    Chip editButton;
+
+    Button attendButton;
+
+    Boolean isAttended = false;
 
     public PostViewHolder(@NotNull View view, PostsRecyclerAdapter.OnItemClickListener listener, List<Post> data) {
         super(view);
+
         postTitle = view.findViewById(R.id.post_title);
         postContent = view.findViewById(R.id.post_content);
+        postUserName = view.findViewById(R.id.post_user_name);
         postImage = view.findViewById(R.id.post_image);
-
+        editButton = view.findViewById(R.id.post_edit_button);
+        userAvatar = view.findViewById(R.id.post_user_avatar);
+        attendButton = view.findViewById(R.id.post_attend);
         this.data = data;
+
+
         view.setOnClickListener(view1 -> {
             int pos = getAdapterPosition();
             listener.onItemClick(pos);
         });
     }
 
-    public void bind(Post post) {
+    public void bind(Post post, User currentUser) {
+
         postTitle.setText(post.getTitle());
         postContent.setText(post.getContent());
+        postUserName.setText(post.getCreatorName());
+        Picasso.get().load(currentUser.getAvatarUrl()).placeholder(R.drawable.undraw_pic_profile_re_7g2h).into(userAvatar);
+        String participants = post.getParticipants();
         if (!Objects.equals(post.getImageUrl(), "")) {
-            Picasso.get().load(post.getImageUrl()).placeholder(R.drawable.undraw_grandma_re_rnv1).into(postImage);
+            Picasso.get().load(post.getImageUrl()).placeholder(R.drawable.progress_animation).into(postImage);
         } else {
-            postImage.setImageResource(R.drawable.undraw_grandma_re_rnv1);
+            postImage.setVisibility(View.GONE);
         }
+
+        if (post.getCreatorId().equals(currentUser.getId())) {
+            editButton.setVisibility(View.VISIBLE);
+        }
+
+        String[] participantsArray = participants.split(",");
+
+        for (String id : participantsArray) {
+            if (id.equals(currentUser.getId())) {
+                isAttended = true;
+                attendButton.setText(R.string.cancel_attend);
+                break;
+            }
+
+        }
+
+        attendButton.setOnClickListener(view -> {
+            String newParticipants = "";
+            if (isAttended) {
+                Stream<String> newParticipantsStream = Arrays.stream(participantsArray).filter(participant -> !participant.equals(currentUser.getId()));
+                String[] arr = (newParticipantsStream.toArray(String[]::new));
+                newParticipants =String.join(",",arr);
+            } else {
+                newParticipants = participants.trim().equals("") ? currentUser.getId() : participants + "," + currentUser.getId();
+            }
+            post.setParticipants(newParticipants);
+            PostModel.getInstance().updatePost(post, data1 -> {
+
+            });
+        });
+
     }
 }
 
@@ -60,9 +115,12 @@ public class PostsRecyclerAdapter extends RecyclerView.Adapter<PostViewHolder> {
     LayoutInflater inflater;
     List<Post> data;
 
+    User currentUser;
+
     public PostsRecyclerAdapter(LayoutInflater inflater, List<Post> data) {
         this.inflater = inflater;
         this.data = data;
+        this.currentUser = User.getCurrentUser();
     }
 
     public List<Post> getData() {
@@ -89,7 +147,7 @@ public class PostsRecyclerAdapter extends RecyclerView.Adapter<PostViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
         Post post = data.get(position);
-        holder.bind(post);
+        holder.bind(post, currentUser);
     }
 
     @Override
--- app/src/main/java/com/example/wegive/models/post/AppLocalDB.java
+++ app/src/main/java/com/example/wegive/models/post/AppLocalDB.java
@@ -3,11 +3,12 @@ package com.example.wegive.models.post;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 
 import com.example.wegive.MyApplication;
 
-@Database(entities = {Post.class}, version = 80)
+@Database(entities = {Post.class}, version = 80,exportSchema = false)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract PostDao postDao();
 }
