a88a88e3d012143d018381283ce046c4d095b377
Omer Kaplan <73556089+Omer-kaplan@users.noreply.github.com>
2023-03-18 20:17:52 +0200

    add fragment files

---
+++ app/src/main/java/com/example/wegive/fragments/account/AccountFragment.java
@@ -0,0 +1,31 @@
+package com.example.wegive.fragments.account;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wegive.R;
+
+
+public class AccountFragment extends Fragment {
+
+    public AccountFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
